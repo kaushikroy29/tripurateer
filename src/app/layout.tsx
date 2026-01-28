479fd6467faa0c4f39ec78d9612b0e991b5e5291
Kaushik Roy <kaushikroy29@gmail.com>
2026-01-28 16:22:02 +0600

    feat: add vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export const metadata: Metadata = {
   },
 };
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,6 +34,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
